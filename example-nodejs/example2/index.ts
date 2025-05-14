eedf54e6ce4cbe746ce66b3d857097d502217ae6
ics-ikeda <ikeda@ics-web.jp>
2025-05-14 23:58:08 +0900

    コードの整合性調整

--- example-nodejs/example2/index.ts
+++ example-nodejs/example2/index.ts
@@ -1,10 +1,17 @@
+// 気象庁の指定地域の天気予報の概要を取得するURL
 const url = "https://www.jma.go.jp/bosai/forecast/data/overview_forecast/130000.json";
 
+/**
+ * 天気予報データを取得し、整形してコンソールに出力します。
+ */
 async function start() {
+  // URLからデータを非同期で取得
   const response = await fetch(url);
-  const html = await response.json();
+  // 取得したレスポンスをJSON形式に変換
+  const data = await response.json();
 
-  console.log(JSON.stringify(html, null, 2));
+  // 取得したJSONデータを整形してコンソールに出力
+  console.log(JSON.stringify(data, null, 2));
 }
 
 start();
--- example-nodejs/example3/index.ts
+++ example-nodejs/example3/index.ts
@@ -1,10 +1,18 @@
+// 外部ファイルを読み込む
 import {TARGET_URL} from './config.ts';
 
+/**
+ * 天気予報データを取得し、整形してコンソールに出力します。
+ * @param url 取得するURL
+ */
 async function start(url:string) {
+  // URLからデータを非同期で取得
   const response = await fetch(url);
-  const html = await response.json();
+  // 取得したレスポンスをJSON形式に変換
+  const data = await response.json();
 
-  console.log(JSON.stringify(html, null, 2));
+  // 取得したJSONデータを整形してコンソールに出力
+  console.log(JSON.stringify(data, null, 2));
 }
 
 start(TARGET_URL);
